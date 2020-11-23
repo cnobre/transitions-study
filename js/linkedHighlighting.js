3ff4a141232698b1ad92d082c82bf06e65c26ede
Christina <ginihumer96@yahoo.de>
2020-11-23 17:33:16 +0100

    update design

--- js/linkedHighlighting.js
+++ js/linkedHighlighting.js
@@ -22,30 +22,31 @@ class LinkedHighlighting {
 
     initVis(){
         var vis = this;
-        this.bar = false;
 
 
         vis.scatterPlot = new ScatterPlot(vis.parentElement + "_scatter", vis.data);
         console.log(vis.scatterPlot);
         vis.scatterPlot.svg.selectAll("circle")
             .on("mouseover", function(d, i){
+                vis.scatterPlot.svg.selectAll("circle").classed('highlight', false);
                 d3.select(this).classed('highlight', true);
                 vis.barChart.highlight([i]);
             })
             .on("mouseout", function(){
-                d3.select(this).classed('highlight', false);
-                vis.barChart.highlight([]);
+                // d3.select(this).classed('highlight', false);
+                // vis.barChart.highlight([]);
             });
 
         vis.barChart = new BarChart(vis.parentElement + "_bar", vis.data);
         vis.barChart.svg.selectAll("rect")
             .on("mouseover", function(d, i){
+                vis.barChart.svg.selectAll("rect").classed('highlight', false);
                 d3.select(this).classed('highlight', true);
                 vis.scatterPlot.highlight([i]);
             })
             .on("mouseout", function(){
-                d3.select(this).classed('highlight', false);
-                vis.scatterPlot.highlight([]);
+                // d3.select(this).classed('highlight', false);
+                // vis.scatterPlot.highlight([]);
                     
             });
     }
